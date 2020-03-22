bec3079a2e145c681e2786b2a4c627cb3055f98e
Tsvetomira Dimitrova <Tsvetomira.Dimitrova@docuware.com>
2020-03-22 22:44:43 +0200

    Alcohol Market

--- AlcoholMarket/Program.cs
+++ AlcoholMarket/Program.cs
@@ -6,9 +6,24 @@ namespace AlcoholMarket
     {
         static void Main(string[] args)
         {
-            double whiskey
+            double whiskeyPrice = double.Parse(Console.ReadLine());
+            double beerVolume = double.Parse(Console.ReadLine());
+            double wineVolume = double.Parse(Console.ReadLine());
+            double rakiaVolume = double.Parse(Console.ReadLine());
+            double whiskeyVolume = double.Parse(Console.ReadLine());
 
-            Console.WriteLine("Hello World!");
+            double rakiaPrice = whiskeyPrice / 2;
+            double winePrice = rakiaPrice - (rakiaPrice * 0.40);
+            double beerPrice = rakiaPrice - (rakiaPrice * 0.80);
+
+            double beerAmount = beerPrice * beerVolume;
+            double wineAmount = winePrice * wineVolume;
+            double rakiaAmount = rakiaPrice * rakiaVolume;
+            double whiskeyAmount = whiskeyPrice * whiskeyVolume;
+
+            double totalPrice = beerAmount + wineAmount + rakiaAmount + whiskeyAmount;
+
+            Console.WriteLine($"{totalPrice:f2}");
         }
     }
 }
